32d69b7146d3566ede81dabe30d4d9032c5613d1
Everybodycries <527bcyxhtg@privaterelay.appleid.com>
2026-04-17 13:13:03 +0200

    Update taskDelete.ts
    feat: ProductDelete endpoint

--- src/endpoints/tasks/taskDelete.ts
+++ src/endpoints/tasks/taskDelete.ts
@@ -1,9 +1,9 @@
 import { D1DeleteEndpoint } from "chanfana";
 import { HandleArgs } from "../../types";
-import { TaskModel } from "./base";
+import { ProductModel } from "./base";
 
-export class TaskDelete extends D1DeleteEndpoint<HandleArgs> {
+export class ProductDelete extends D1DeleteEndpoint<HandleArgs> {
 	_meta = {
-		model: TaskModel,
+		model: ProductModel,
 	};
 }
